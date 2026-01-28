7294635fd3d83f5efb4dc2c5dc675ce3c382a552
ramij333 <skramijsc@gmail.com>
2026-01-28 23:52:21 +0530

    Day 5: handle query parameters

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const http = require("http");
+const url = require("url");
+
 const server = http.createServer((req,res) => {
+    let parsedUrl =  url.parse(req.url, true)
+    let pathName = parsedUrl.pathname
+    let NameValueInQuery = parsedUrl.query.name
+    
     if(req.url === '/' && req.method === 'GET') {
         res.writeHead(200, {"Content-Type" : "text/plain"})
         res.end('Home')
@@ -9,7 +15,20 @@ const server = http.createServer((req,res) => {
         res.writeHead(200, {"Content-Type" : 'application/json'})
         res.end(JSON.stringify({"status" : "ok"}))
         return
-    } else {
+    }
+    else if(pathName === '/greet' && req.method === 'GET') {
+        if(NameValueInQuery) {
+            res.writeHead(200,{"content-type" : "text/plain"})
+            res.end(`Hello ${NameValueInQuery}`)
+            return
+        } else {
+            res.writeHead(400, {"content-type" : "text/plain"})
+            res.end("Name is required")
+            return
+        }
+       
+    }
+    else {
         res.writeHead(404, {"Content-Type" : "text/plain"})
         res.end("Not Found")
         return
